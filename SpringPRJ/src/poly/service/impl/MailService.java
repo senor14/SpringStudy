7fdfb0cf1ff3aa3bca489fe0e4a2fb4b11ba37be
senor14 <64997244+senor14@users.noreply.github.com>
2020-09-21 12:37:01 +0900

    Update MailService.java

--- SpringPRJ/src/poly/service/impl/MailService.java
+++ SpringPRJ/src/poly/service/impl/MailService.java
@@ -24,8 +24,8 @@ public class MailService implements IMailService {
 	private Logger log = Logger.getLogger(this.getClass());
 	
 	final String host = "smtp.naver.com"; //네이버에서 제공하는 SMTP 서버
-	//final String user = "polytech_data16@naver.com"; // 본인 네이버 아이디
-	//final String password = "vhfflxpr"; // 본인 네이버 비밀번호
+	//final String user = ""; // 본인 네이버 아이디
+	//final String password = ""; // 본인 네이버 비밀번호
 	
 	@Override
 	public int doSendMail(MailDTO pDTO) {
