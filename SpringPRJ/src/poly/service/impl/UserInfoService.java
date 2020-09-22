a5568555e83699a50d99e1cc011ee36e25e2e96b
Senor <dkssud940@gmail.com>
2020-09-22 11:06:42 +0900

    userinfoservice

--- SpringPRJ/src/poly/service/impl/UserInfoService.java
+++ SpringPRJ/src/poly/service/impl/UserInfoService.java
@@ -4,16 +4,23 @@ import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
 
+import poly.dto.MailDTO;
 import poly.dto.UserInfoDTO;
 import poly.persistance.mapper.IUserInfoMapper;
+import poly.service.IMailService;
 import poly.service.IUserInfoService;
 import poly.util.CmmUtil;
+import poly.util.EncryptUtil;
 
 @Service("UserInfoService")
 public class UserInfoService implements IUserInfoService {
 	
 	@Resource(name="UserInfoMapper")
 	private IUserInfoMapper userInfoMapper;
+	
+	// 메일 발송을 위한 MailService 자바 객체 가져오기
+	@Resource(name = "MailService")
+	private IMailService mailService;
 	
 	@Override
 	public int insertUserInfo(UserInfoDTO pDTO) throws Exception {
@@ -43,9 +50,35 @@ public class UserInfoService implements IUserInfoService {
 			// 회원가입
 			int success = userInfoMapper.insertUserInfo(pDTO);
 			
-			// db에 데이터가 등록되었다면,
+			// db에 데이터가 등록되었다면(회원ㄱ아비 성공했다면....
 			if (success > 0) {
 				res = 1;
+				
+				/*
+				 * ################################################################
+				 * 						메일 발송 로직 추가 시작!!
+				 * ################################################################
+				 */
+				
+				MailDTO mDTO= new MailDTO();
+				
+				// 회원정보화면에서 입력받은 이메일 변수(아직 암호화되어 넣어오기 떄문에 복호화 수행함)
+				mDTO.setToMail(EncryptUtil.decAES128CBC(CmmUtil.nvl(pDTO.getEmail())));
+				
+				mDTO.setTitle("회원가입을 축하드립니다."); //제목
+				
+				// 메일 내용에 가입자 이름 넣어서 내용 발송
+				mDTO.setContents(CmmUtil.nvl(pDTO.getUser_name()) + "님의 회원가입을 진심으로 축하드립니다.");
+				
+				// 회원 가입이 성공했기 때문에 메일을 발송함
+				mailService.doSendMail(mDTO);
+				
+				/*
+				 * ################################################################
+				 * 						메일 발송 로직 추가 끝!!
+				 * ################################################################
+				 */
+				
 			} else {
 				res = 0;
 			}
