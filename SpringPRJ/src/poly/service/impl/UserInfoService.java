cb4265090daf076c7b1c81f4019f7bda020cbfc8
Senor <dkssud940@gmail.com>
2020-09-30 22:57:13 +0900

    .

--- SpringPRJ/src/poly/service/impl/UserInfoService.java
+++ SpringPRJ/src/poly/service/impl/UserInfoService.java
@@ -10,6 +10,7 @@ import poly.persistance.mapper.IUserInfoMapper;
 import poly.service.IMailService;
 import poly.service.IUserInfoService;
 import poly.util.CmmUtil;
+import poly.util.DateUtil;
 import poly.util.EncryptUtil;
 
 @Service("UserInfoService")
@@ -44,6 +45,7 @@ public class UserInfoService implements IUserInfoService {
 		// 중복된 회원정보가 있는 겨우, 결과값을 2로 변경하고, 더 이상 작업을 진행하지 않음
 		if (CmmUtil.nvl(rDTO.getExists_yn()).equals("Y")) {
 			res = 2;
+			
 		// 회원가입이 중복이 아니므로, 회원가입 진행함
 		} else {
 			
@@ -83,6 +85,76 @@ public class UserInfoService implements IUserInfoService {
 				res = 0;
 			}
 		}
+		
+		return res;
+	}
+
+	/**
+	 * 로그인을 위해 아이디와 비밀번호가 일치하는지 확인하기
+	 * 
+	 * @param UserInfoDTO 로그인을 위한 회원아이디, 비밀번호
+	 * @return UserInfoDTO 로그인된 회원아이디 정보
+	 */
+	@Override
+	public int getUserLoginCheck(UserInfoDTO pDTO) throws Exception {
+		
+		// 로그인 성공 : 1, 실패 : 0
+		int res = 0;
+		
+		// 로그인을 위해 아이디와 비밀번호가 일치하는지 확인하기 위한 mapper 호출하기
+		UserInfoDTO rDTO = userInfoMapper.getUserLoginCheck(pDTO);
+		
+		if (rDTO == null) {
+			rDTO = new UserInfoDTO();
+		}
+		
+		/*
+		 * ##########################################################
+		 * 					로그인 성공 여부 체크 시작!
+		 * ##########################################################
+		 */
+		
+		/*
+		 * userInfoMapper로 부터 SELECT 쿼리의 결과로 회원아이디를 받아왔다면, 로그인 성공!!
+		 * 
+		 * DTO의 변수에 값이 있는지 확인하는데 처리속도 측면에서 가장 좋은 방법은 변수의 길이를 가져오는 것이다.
+		 * 따라서 .length() 함수를 통해 회원아이디의 글자수를 가져와 0보다 큰지 비교한다.
+		 * 0보다 크다면, 글자가 존재하는 것이기 때문에 값이 존재한다.
+		 */
+		if (CmmUtil.nvl(rDTO.getUser_id()).length()>0) {
+			res = 1;
+			
+			/*
+			 * #######################################################
+			 * 					메일 발송 로직 추가 시작!
+			 * #######################################################
+			 */
+			
+			MailDTO mDTO = new MailDTO();
+			
+			// 아이디, 패스워드 일치하는지 체크하는 쿼리에서 이메일 값 받아오기(아직 암호화되어 넘어오기 때문에 복호화 수행함)
+			mDTO.setToMail(EncryptUtil.decAES128CBC(CmmUtil.nvl(rDTO.getEmail())));
+			
+			mDTO.setTitle("로그인 알림!"); //제목
+			
+			// 메일 내용에 가입자 이름 넣어서 내용 발송
+			mDTO.setContents(DateUtil.getDateTime("yyyy.MM.dd 24h:mm:ss") + "에 " + CmmUtil.nvl(rDTO.getUser_name()) + "님이 로그인 하였습니다.");
+			
+			// 회원 가입이 성공했기 때문에 메일을 발송함
+			mailService.doSendMail(mDTO);
+			
+			/*
+			 * #######################################################
+			 * 					메일 발송 로직 추가 끝!
+			 * #######################################################
+			 */
+		}
+		
+		/*
+		 * ###########################################################
+		 * 					로그인 성공 여부 체크 끝!
+		 * ###########################################################
+		 */
 		
 		return res;
 	}
